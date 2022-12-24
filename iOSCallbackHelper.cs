3def2270a922abc13eb3091d70869b3eb01ce32e
FengYan <68401855+Aaron8052@users.noreply.github.com>
2022-12-23 21:12:20 -0500

    Add files via upload

---
+++ iOSCallbackHelper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class iOSCallbackHelper : MonoBehaviour {
+    static iOSCallbackHelper instance;
+    static public iOSCallbackHelper INSTANCE
+    {
+        get
+        {
+            if(!instance) instance = FindObjectOfType<iOSCallbackHelper>() ?? 
+                    new GameObject("iOSCallbackHelper").AddComponent<iOSCallbackHelper>();
+            return instance;
+        }
+    }
+
+    Action onShareCloseCallback;
+
+    public void SetShareCloseCallback(Action callback)
+    {
+        onShareCloseCallback = callback;
+    }
+
+    //This method will be called from iOSNative Objective-C codes!!!
+    public void OnShareCloseCallback(string msg)
+    {
+        if(msg.Equals("Closed") && onShareCloseCallback != null)
+        {
+            onShareCloseCallback.Invoke();
+        }
+    }
+
+}
