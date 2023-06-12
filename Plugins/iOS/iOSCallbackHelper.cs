ca4bb0516de94d770a8b40b86b81e379375b59d2
FengYan <jiangyu8052@gmail.com>
2023-06-12 09:44:39 +0800

    将插件放入iOSNativePlugin命名空间下

--- Plugins/iOS/iOSCallbackHelper.cs
+++ Plugins/iOS/iOSCallbackHelper.cs
@@ -3,82 +3,87 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class iOSCallbackHelper : MonoBehaviour {
-    static iOSCallbackHelper instance;
-    static public iOSCallbackHelper INSTANCE
+namespace iOSNativePlugin
+{
+    public class iOSCallbackHelper : MonoBehaviour 
     {
-        get
+        static iOSCallbackHelper instance;
+        static public iOSCallbackHelper INSTANCE
         {
-            if(!instance) {
-                instance = FindObjectOfType<iOSCallbackHelper>() ?? 
-                    new GameObject("iOSCallbackHelper").AddComponent<iOSCallbackHelper>();
-                DontDestroyOnLoad(instance);
+            get
+            {
+                if(!instance) {
+                    instance = FindObjectOfType<iOSCallbackHelper>() ?? 
+                               new GameObject("iOSCallbackHelper").AddComponent<iOSCallbackHelper>();
+                    DontDestroyOnLoad(instance);
+                }
+                return instance;
             }
-            return instance;
         }
-    }
 
-    event Action OnShareClose;
+        event Action OnShareClose;
 
-    public void SetShareCloseCallback(Action callback)
-    {
-        OnShareClose = callback;
-    }
-
-    //This method will be called from iOSNative Objective-C codes!!!
-    public void OnShareCloseCallback(string msg)
-    {
-        if(msg.Equals("Closed") && OnShareClose != null)
+        public void SetShareCloseCallback(Action callback)
         {
-            OnShareClose.Invoke();
-            OnShareClose = null;
+            OnShareClose = callback;
         }
-    }
+
+        //This method will be called from iOSNative Objective-C codes!!!
+        public void OnShareCloseCallback(string msg)
+        {
+            if(msg.Equals("Closed") && OnShareClose != null)
+            {
+                OnShareClose.Invoke();
+                OnShareClose = null;
+            }
+        }
     
-    event Action OnFileSaved;
-    public void SetSaveFileCallback(Action callback)
-    {
-        OnFileSaved = callback;
-    }
+        event Action OnFileSaved;
+        public void SetSaveFileCallback(Action callback)
+        {
+            OnFileSaved = callback;
+        }
 
-    public void OnFileSaveCallback(string msg)
-    {
-        if (msg.Equals("True") && OnFileSaved != null)
+        public void OnFileSaveCallback(string msg)
         {
-            OnFileSaved.Invoke();
+            if (msg.Equals("True") && OnFileSaved != null)
+            {
+                OnFileSaved.Invoke();
+            }
+            else
+            {
+                OnFileSaved = null;
+            }
         }
-        else
-        {
-            OnFileSaved = null;
-        }
-    }
     
-    event Action<string> OnFileSelected;
-    event Action OnFileSelectFailed;
-    public void SetFileSelectedCallback(Action<string> callback)
-    {
-        OnFileSelected = callback;
-    }
-    public void SetFileSelectedFailedCallback(Action callback)
-    {
-        OnFileSelectFailed = callback;
-    }
-    public void OnFileSelectedCallback(string msg)
-    {
-        if (OnFileSelected != null)
+        event Action<string> OnFileSelected;
+        event Action OnFileSelectFailed;
+        public void SetFileSelectedCallback(Action<string> callback)
         {
-            OnFileSelected.Invoke(msg);
-            OnFileSelected = null;
+            OnFileSelected = callback;
         }
+        public void SetFileSelectedFailedCallback(Action callback)
+        {
+            OnFileSelectFailed = callback;
+        }
+        public void OnFileSelectedCallback(string msg)
+        {
+            if (OnFileSelected != null)
+            {
+                OnFileSelected.Invoke(msg);
+                OnFileSelected = null;
+            }
 
-    }
-    public void OnFileSelectedFailedCallback(string msg)
-    {
-        if (OnFileSelectFailed != null)
-        {
-            OnFileSelectFailed.Invoke();
-            OnFileSelectFailed = null;
         }
+        public void OnFileSelectedFailedCallback(string msg)
+        {
+            if (OnFileSelectFailed != null)
+            {
+                OnFileSelectFailed.Invoke();
+                OnFileSelectFailed = null;
+            }
         
+        }
     }
 }
+
