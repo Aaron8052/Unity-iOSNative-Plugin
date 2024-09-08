09fe8a9062cad8a824cf9ca19bb54e7d9bfe9f8d
FengYan <jiangyu8052@gmail.com>
2024-09-08 09:05:47 -0400

    CloudKit开发

---
+++ Plugins/iOS/CloudKit/Native/CloudKit.mm
@@ -0,0 +1,14 @@
+#import <CloudKit/CloudKit.h>
+
+extern "C"
+{
+
+CKRecord* InitCKRecord(const char* recordType){
+    CKRecord* record = [[CKRecord alloc] initWithRecordType:[NSString stringWithUTF8String:recordType]];
+    return [record retain]; // 防止指针被回收
+}
+
+void ReleaseCKRecord(CKRecord* handle){
+    delete handle;
+}
+}
