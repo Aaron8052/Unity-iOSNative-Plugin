8af4e4f7d03b41b4caf912072620cb200091a350
FengYan <68401855+Aaron8052@users.noreply.github.com>
2025-01-14 09:29:01 +0800

    Keyboard

--- Plugins/CS/Keyboard.cs
+++ Plugins/CS/Keyboard.cs
@@ -9,6 +9,13 @@ namespace iOSNativePlugin
 {
     public static class Keyboard
     {
+        static readonly KeyCode[] GCKeyCodeMatch =
+        {
+            KeyCode.A, KeyCode.B, KeyCode.C, KeyCode.D, KeyCode.E, KeyCode.F, KeyCode.G, KeyCode.H, KeyCode.I,
+            KeyCode.J, KeyCode.K, KeyCode.L, KeyCode.M, KeyCode.N, KeyCode.O, KeyCode.P, KeyCode.Q, KeyCode.R,
+            KeyCode.S, KeyCode.T, KeyCode.U, KeyCode.V, KeyCode.W, KeyCode.X, KeyCode.Y, KeyCode.Z,
+            //TODO: ...
+        };
         [DllImport("__Internal")] static extern void Keyboard_RegisterKeyPressCallback(LongCallback callback);
         [DllImport("__Internal")] static extern void Keyboard_RegisterKeyReleaseCallback(LongCallback callback);
         [DllImport("__Internal")] static extern bool Keyboard_IsKeyboardSupported();
@@ -17,29 +24,24 @@ namespace iOSNativePlugin
         public static event Action<KeyCode> OnKeyPressed;
         public static event Action<KeyCode> OnKeyReleased;
 
+        [RuntimeInitializeOnLoadMethod]
         static void Init()
         {
-
+            Keyboard_RegisterKeyPressCallback(OnKeyPressedCallback);
+            Keyboard_RegisterKeyReleaseCallback(OnKeyReleasedCallback);
         }
 
         [MonoPInvokeCallback(typeof(LongCallback))]
         static void OnKeyPressedCallback(long GCKeyCode)
         {
-            OnKeyPressed?.Invoke();
+            OnKeyPressed?.Invoke(GCKeyCodeMatch[GCKeyCode]);
         }
 
         [MonoPInvokeCallback(typeof(LongCallback))]
         static void OnKeyReleasedCallback(long GCKeyCode)
         {
-            OnKeyReleased?.Invoke();
+            OnKeyReleased?.Invoke(GCKeyCodeMatch[GCKeyCode]);
         }
 
-        static KeyCode GetKeyCodeFromGCKeyCode(long GCKeyCode)
-        {
-            return GCKeyCode switch
-            {
-
-            }
-        }
     }
 }
